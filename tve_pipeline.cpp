449037871aae5fe5507228e30eb4046b46ceb895
Mishal-Alajmi <mishalajmi.dev@gmail.com>
2023-12-05 17:05:03 +0300

    test commit

--- tve_pipeline.cpp
+++ tve_pipeline.cpp
@@ -1,41 +1,38 @@
 #include "tve_pipeline.hpp"
 
 #include <fstream>
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 
-namespace tve
-{
-    TvePipeline::TvePipeline(const std::string &vertFilepath, const std::string &fragFilepath)
-    {
-        createGraphicsPipeline(vertFilepath, fragFilepath);
-    }
-
-    std::vector<char> TvePipeline::readFile(const std::string &filepath)
-    {
-        std::ifstream file{filepath, std::ios::ate | std::ios::binary};
-
-        if (!file.is_open())
-        {
-            throw std::runtime_error("Failed to open file: " + filepath);
-        }
-
-        size_t fileSize = static_cast<size_t>(file.tellg());
-        std::vector<char> buffer(fileSize);
-
-        file.seekg(0);
-        file.read(buffer.data(), fileSize);
-
-        file.close();
-        return buffer;
-    }
-
-    void TvePipeline::createGraphicsPipeline(const std::string &vertFilePath, const std::string &fragFilePath)
-    {
-        auto vertCode = readFile(vertFilePath);
-        auto fragCode = readFile(fragFilePath);
-
-        std::cout << "Vertex Shader Code Size: " << vertCode.size() << "\n";
-        std::cout << "Fragment Shader Code Size: " << fragCode.size() << "\n";
-    }
+namespace tve {
+TvePipeline::TvePipeline(const std::string &vertFilepath,
+                         const std::string &fragFilepath) {
+  createGraphicsPipeline(vertFilepath, fragFilepath);
 }
+
+std::vector<char> TvePipeline::readFile(const std::string &filepath) {
+  std::ifstream file{filepath, std::ios::ate | std::ios::binary};
+
+  if (!file.is_open()) {
+    throw std::runtime_error("Failed to open file: " + filepath);
+  }
+
+  size_t fileSize = static_cast<size_t>(file.tellg());
+  std::vector<char> buffer(fileSize);
+
+  file.seekg(0);
+  file.read(buffer.data(), fileSize);
+
+  file.close();
+  return buffer;
+}
+
+void TvePipeline::createGraphicsPipeline(const std::string &vertFilePath,
+                                         const std::string &fragFilePath) {
+  auto vertCode = readFile(vertFilePath);
+  auto fragCode = readFile(fragFilePath);
+
+  std::cout << "Vertex Shader Code Size: " << vertCode.size() << "\n";
+  std::cout << "Fragment Shader Code Size: " << fragCode.size() << "\n";
+}
+} // namespace tve
